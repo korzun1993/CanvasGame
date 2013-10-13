42376899682121cde3c6f528c3502d6ed2fb8711
Vlad Korzun <korzun1993@mail.ru>
2013-10-13 09:06:59 +0300

    init commit

---
+++ app/controllers/main_controller_controller.rb
@@ -0,0 +1,9 @@
+class MainControllerController < ApplicationController
+  def index
+
+  end
+
+  def start
+    render :text => "Ok"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Game::Application.config.secret_key_base = '4f5161d76fec35baee44dbf292d6ac94d7982fe83d13afa4ec02510850a631e1ab14fbd7e02ead3f403c796b26d79c65534d043a8835ccd41460b5b65d72b456'
